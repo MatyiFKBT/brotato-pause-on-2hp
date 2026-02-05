3d8b8235b17b57f4c0ba03826421c58217834ce7
MatyiFKBT <6183867+MatyiFKBT@users.noreply.github.com>
2026-02-05 11:02:20 +0100

    fix: update mod name (#2)
    Update name to match functionality of the mod.

--- mods-unpacked/Matyi-PauseOnLethal/mod_main.gd
+++ mods-unpacked/Matyi-PauseOnLethal/mod_main.gd
@@ -1,6 +1,6 @@
 extends Node
 
-const MOD_DIR = "Matyi-PauseOn2hp/"
+const MOD_DIR = "Matyi-PauseOnLethal/"
 
 func _init(modLoader = ModLoader):
 	modLoader.install_script_extension("res://mods-unpacked/" + MOD_DIR + "extensions/entities/units/player/player.gd")
